fdba881e5e544098b83b30fee697ae07bc9afe4b
zhoujingjin <5137845022@qq.com>
2023-08-08 23:10:25 +0800

    fisrt1

--- app/src/main/java/com/best/now/autoclick/ui/MainActivity.kt
+++ app/src/main/java/com/best/now/autoclick/ui/MainActivity.kt
@@ -107,24 +107,6 @@ class MainActivity : BaseVMActivity() {
 
 
             }
-            ivCamera.setOnClickListener {
-                if (isPurchased(this@MainActivity)){
-                    PermissionX.init(this@MainActivity)
-                        .permissions(Manifest.permission.RECORD_AUDIO, Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        .request { allGranted, _, deniedList ->
-                            if (allGranted) {
-                                WebPlayPianoActivity.startActivity(
-                                    this@MainActivity,
-                                    "Camera Translation",
-                                    Constant.URL_PIANO
-                                )
-                            } else {
-                                ToastUtils.showShort("These permissions are denied: $deniedList")
-                            }
-                        }
-                }
-
-            }
         }
     }
 
