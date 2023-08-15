5e45ad30076878749d0a71187c34c06030b277f8
zhoujingjin <5137845022@qq.com>
2023-08-15 23:22:01 +0800

    Pdffour

---
+++ app/src/main/java/com/best/now/autoclick/ui/FlashlightActivity.kt
@@ -0,0 +1,16 @@
+package com.best.now.autoclick.ui
+
+import com.best.now.autoclick.BaseVMActivity
+
+/**
+author:zhoujingjin
+date:2023/8/15
+ */
+class FlashlightActivity:BaseVMActivity() {
+    override fun initView() {
+
+    }
+
+    override fun initData() {
+    }
+}
