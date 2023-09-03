c604bebe073d37c8bcf78744f20f5701cff266f8
zhoujingjin <5137845022@qq.com>
2023-09-03 22:42:36 +0800

    six

---
+++ app/src/main/java/com/best/now/six/ui/CaseActivity.kt
@@ -0,0 +1,21 @@
+package com.best.now.six.ui
+
+import com.best.now.six.BaseVMActivity
+import com.best.now.six.R
+import com.best.now.six.databinding.ActivityCaseBinding
+import com.best.now.six.databinding.ActivityGuideBinding
+import com.best.now.six.databinding.ActivityNoiseBinding
+
+/**
+author:zhoujingjin
+date:2023/9/3
+ */
+class CaseActivity:BaseVMActivity() {
+    private val binding by binding<ActivityCaseBinding>(R.layout.activity_case)
+    override fun initView() {
+
+    }
+
+    override fun initData() {
+    }
+}
---
+++ app/src/main/java/com/best/now/six/ui/NoiseActivity.kt
@@ -0,0 +1,20 @@
+package com.best.now.six.ui
+
+import com.best.now.six.BaseVMActivity
+import com.best.now.six.R
+import com.best.now.six.databinding.ActivityGuideBinding
+import com.best.now.six.databinding.ActivityNoiseBinding
+
+/**
+author:zhoujingjin
+date:2023/9/3
+ */
+class NoiseActivity:BaseVMActivity() {
+    private val binding by binding<ActivityNoiseBinding>(R.layout.activity_noise)
+    override fun initView() {
+
+    }
+
+    override fun initData() {
+    }
+}
