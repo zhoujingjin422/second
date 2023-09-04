24ce0ad36e72308b6a17dfc1edcc924aa7d23a0f
zhoujingjin <5137845022@qq.com>
2023-09-04 20:59:30 +0800

    six

--- app/src/main/java/com/best/now/six/ui/NoiseActivity.kt
+++ app/src/main/java/com/best/now/six/ui/NoiseActivity.kt
@@ -8,6 +8,7 @@ import com.best.now.six.bean.NoiseBean
 import com.best.now.six.databinding.ActivityNoiseBinding
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
 
 
 /**
@@ -22,13 +23,17 @@ class NoiseActivity:BaseVMActivity() {
         , "Thunder","Sea Wave","Waterfall","Backwash","Heavy Rain","Breeze","Mountain","Light Rain")
     private val soundArr = mutableListOf("白噪音.mp3", "风扇.mp3", "粉色噪音.mp3", "炽热风浪.mp3"
         , "电闪雷鸣.mp3","海浪轻抚.mp3","近闻瀑布.mp3","巨浪涛天.mp3","倾盆大雨.mp3","清风拂面.mp3","深林山风.mp3","小雨淅沥.mp3")
-    private val resIdArr = mutableListOf(R.drawable.guide_1, R.drawable.guide_1,R.drawable.guide_1, R.drawable.guide_1
-        , R.drawable.guide_1,R.drawable.guide_1,R.drawable.guide_1,R.drawable.guide_1,R.drawable.guide_1,
-        R.drawable.guide_1,R.drawable.guide_1,R.drawable.guide_1)
+    private val resIdArr = mutableListOf(R.drawable.iv_1, R.drawable.iv_2,R.drawable.iv_3, R.drawable.iv_4
+        , R.drawable.iv_5,R.drawable.iv_6,R.drawable.iv_7,R.drawable.iv_8,R.drawable.iv_9,
+        R.drawable.iv_10,R.drawable.iv_11,R.drawable.iv_12)
     private var isPlaying = false
     private var nowPlay:NoiseBean? = null
     override fun initView() {
         binding.apply{
+            setSupportActionBar(toolBar)
+            toolBar.setNavigationOnClickListener {
+                finish()
+            }
             recyclerView.layoutManager = GridLayoutManager(this@NoiseActivity,3)
             noiseAdapter = NoiseAdapter {
                 var newPlay = false
@@ -41,6 +46,8 @@ class NoiseActivity:BaseVMActivity() {
                     newPlay = true
                 }
                 playSound(newPlay)
+                noiseAdapter.isPlaying = isPlaying
+                noiseAdapter.notifyDataSetChanged()
             }
             recyclerView.adapter = noiseAdapter
         }
@@ -50,6 +57,14 @@ class NoiseActivity:BaseVMActivity() {
    private fun playSound(newPlay: Boolean) {
        if (player==null){
            player = ExoPlayer.Builder(this).build()
+           player?.addListener(object : Player.Listener{
+               override fun onPlaybackStateChanged(playbackState: Int) {
+                if (playbackState==Player.STATE_ENDED){
+                    player?.seekTo(0)
+                    player?.playWhenReady = true
+                }
+               }
+           })
        }
        if (isPlaying){
            if (newPlay){
